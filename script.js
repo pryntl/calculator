4e45aa06a0c7937392be07bc5cc122addbaf62bc
pryntl <pryntl2007@gmail.com>
2024-08-04 18:06:49 +0330

    feat [java script]
    added new features. for example b-a instead of a-b.

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ let calculator = {
   // mathematics symbols
   let plus = calculator.num1 + calculator.num2,
     minus = calculator.num1 - calculator.num2,
+    minus2 = calculator.num2 - calculator.num1,
     division = calculator.num1 / calculator.num2,
+    division2 = calculator.num2 / calculator.num1,
     multiplication = calculator.num1 * calculator.num2,
     remainder = calculator.num1 % calculator.num2;
     //printing the results
   console.log("numbers:" , calculator.num1, "," ,calculator.num2);
   console.log("plus:", plus);
-  console.log("minus:", minus);
-  console.log("division:", division.toFixed(2));
+  console.log("minus(a-b):", minus);
+  console.log("minus(b-a):", minus2);
+  console.log("division(a/b):", division.toFixed(2));
+  console.log("division (b/a):" , division2);
   console.log("multiplication:", multiplication);
   console.log("remainder:", remainder);
   
